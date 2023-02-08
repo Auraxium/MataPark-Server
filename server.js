2ed358523878400ebf92f6ea477d6413e95a3825
Auraxium <clutchmasterman@gmail.com>
2023-02-08 13:50:40 -0800

    aaa

--- server.js
+++ server.js
@@ -1,86 +1,105 @@
 const express = require("express");
 const app = express();
-const Chromium = require("chromium")
-const path = require("path");
-const fs = require("fs");
 const cors = require("cors");
-const puppeteer = require('puppeteer')
+const puppeteer = require("puppeteer");
 const mongoose = require("mongoose");
-var permitModel = mongoose.model('Permit', new mongoose.Schema({license: mongoose.Mixed, expires: mongoose.Mixed}))
-const URI = "mongodb+srv://Lemond:z6WKxBTkHFuLUEKi@cluster0.cb5agdt.mongodb.net/?retryWrites=true&w=majority"
+const Chromium = require("chromium");
+var permitModel = mongoose.model(
+  "Permit",
+  new mongoose.Schema({ license: mongoose.Mixed, expires: mongoose.Mixed })
+);
+const URI =
+  "mongodb+srv://Lemond:z6WKxBTkHFuLUEKi@cluster0.cb5agdt.mongodb.net/?retryWrites=true&w=majority";
 
-app.use(cors({ 
-	origin: "*", 
-	credentials: true,
-	methods: "*",
-	headers: "*"
-}));
+//	Chromium.install().then(() => parkingUpdate())
+//	parkingUpdate();
+
+	 
+
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+    methods: "*",
+    headers: "*",
+  })
+);
 app.use(express.json());
 
-mongoose.connect(URI, {
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
-
-})
-.then(() => console.log("connected to database"))
-.catch((err) => console.log(err));
+mongoose
+  .connect(URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("connected to database"))
+  .catch((err) => console.log(err));
 
 let lot_global = {};
 
 async function parkingUpdate() {
-	try {
-		console.log('--------------')
-	const browser = await puppeteer.launch();
-	console.log('launched')
-	const page = await browser.newPage();
-	console.log('new page')
+  try {
+    console.log("--------------");
+    const browser = await puppeteer.launch();
+    console.log("launched");
+    const page = await browser.newPage();
+    console.log("new page");
 
-	await page.goto('https://m.csun.edu/alumni_community/find_parking/index');
-	console.log('on page')
+    await page.goto("https://m.csun.edu/alumni_community/find_parking/index");
+    console.log("on page");
 
-const lots = await page.evaluate(() => 
-		Array.from(document.querySelectorAll('tr.kgoui_object.kgoui_table_table_row'), (e) => ({
-			lot: e.querySelector(':first-child div a div .kgo-title :first-child strong').innerText,
-			slots: e.querySelector(':nth-child(2) div a div .kgo-title :first-child span').innerText
-		}))
-	)
+    const lots = await page.evaluate(() =>
+      Array.from(
+        document.querySelectorAll("tr.kgoui_object.kgoui_table_table_row"),
+        (e) => ({
+          lot: e.querySelector(
+            ":first-child div a div .kgo-title :first-child strong"
+          ).innerText,
+          slots: e.querySelector(
+            ":nth-child(2) div a div .kgo-title :first-child span"
+          ).innerText,
+        })
+      )
+    );
 
-	console.log('got lots');
-	lot_global = lots;
+    console.log("got lots");
+    lot_global = lots;
 
-	await browser.close();
+    await browser.close();
 
-	console.log('closed')
-	} catch(err) {
-		console.log(err);
-		await browser.close();
-	}
-	
+    console.log("closed");
+  } catch (err) {
+    console.log(err);
+    await browser.close();
+  }
 }
 
 parkingUpdate();
-let interval = setInterval(parkingUpdate, 9*60*1000);
+let interval = setInterval(parkingUpdate, 9 * 1000 * 60);
 
 app.get("/load", (req, res) => {
-	permitModel.find().then(arr => res.json(arr))
-})
+  permitModel.find().then((arr) => res.json(arr));
+});
 
 app.get("/parking-availability", async (req, res) => {
-	return res.json(lot_global);
-})
+  return res.json(lot_global);
+});
 
 app.post("/save", (req, res) => {
-	console.log(req.body)
-	let permit = new permitModel(req.body);
-	console.log(permit) 
-	permit.save().then(() => res.json('Permit added'))
-  .catch(err => res.status(400).json('Error: ' + err));
-
+  console.log(req.body);
+  let permit = new permitModel(req.body);
+  console.log(permit);
+  permit
+    .save()
+    .then(() => res.json("Permit added"))
+    .catch((err) => res.status(400).json("Error: " + err));
 });
 
 app.delete("/delete/:id", (req, res) => {
-	console.log("test");
-	permitModel.findByIdAndDelete(req.params.id).then(() => console.log("deleted permit")).catch((err) => console.log(err))
-})
+  console.log("test");
+  permitModel
+    .findByIdAndDelete(req.params.id)
+    .then(() => console.log("deleted permit"))
+    .catch((err) => console.log(err));
+});
 
 app.listen(8080, null, () => console.log("Running"));
