159e2a14d65e3c675b16f376069d6465f2847112
Auraxium <clutchmasterman@gmail.com>
2023-03-28 14:19:42 -0700

    for

--- server.js
+++ server.js
@@ -2,22 +2,23 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const axios = require("axios");
-const puppeteer = require("puppeteer");
 const mongoose = require("mongoose");
-const chromium = require("chromium");
-var permitModel = mongoose.model(
+const { spawn } = require("child_process");
+
+const permitModel = mongoose.model(
   "Permit",
   new mongoose.Schema({ license: mongoose.Mixed, expires: mongoose.Mixed })
 );
+
 const URI =
   "mongodb+srv://Lemond:z6WKxBTkHFuLUEKi@cluster0.cb5agdt.mongodb.net/?retryWrites=true&w=majority";
 
-let lot_global = [];
+let park_data = {
+  lot: [],
+  now: 0
+};
 
-parkingUpdate()
-let interval = setInterval(parkingUpdate, 9 * 1000 * 60);
-
-let randomArray = [3];
+parkUpdate();
 
 app.use(
   cors({
@@ -37,77 +38,28 @@ mongoose
   .then(() => console.log("connected to database"))
   .catch((err) => console.log(err));
 
-async function parkingUpdate() {
-  try {
-
-    axios
-    .post("https://mpwake.onrender.com//req1", {
-      value: Math.random() * randomArray[0],
-    })
-    .then((data) => (randomArray[0] -= Math.random() * data.data["value"]))
-    .catch((err) => console.log('err'))
-    .finally(() =>  console.log('lot tested'));
-
-    console.log("--------------");
-    const browser = await puppeteer.launch({
-      executablePath: chromium.path,
-      headless: true,
-    });
-    console.log("launched");
-    const page = await browser.newPage();
-    console.log("new page");
-
-    await page.goto("https://m.csun.edu/alumni_community/find_parking/index");
-    console.log("on page");
-
-    const lots = await page.evaluate(() =>
-      Array.from(
-        document.querySelectorAll("tr.kgoui_object.kgoui_table_table_row"),
-        (e) => ({
-          lot: e.querySelector(
-            ":first-child div a div .kgo-title :first-child strong"
-          ).innerText,
-          slots: e.querySelector(
-            ":nth-child(2) div a div .kgo-title :first-child span"
-          ).innerText,
-        })
-      )
-    );
-
-    console.log("got lots");
-    lot_global = lots;
-
-    await browser.close();
-
-    console.log("closed");
-		
-  } catch (err) {
-    console.log(err);
-    await browser.close();
-  }
+function parkUpdate() {
+  const py = spawn("python", ["./parkingUpdate.py"]);
+  py.stdout.on("data", (data) => {
+    try {
+      park_data = JSON.parse(data.toString());
+      return park_data;
+    } catch (err){
+      console.log(err)
+      res.status(500).send(err);
+    }
+  });
 }
 
 app.get("/load", (req, res) => {
   permitModel.find().then((arr) => res.json(arr));
 });
 
-app.post("/req1", (req, res) => {
-  let val = req.body["value"];
-  randomArray = [Math.random() * val];
-  console.log('test')
-  axios
-    .post("https://mpwake.onrender.com/req1", {
-      value: Math.random() * randomArray[0],
-    })
-    .then((data) => (randomArray[0] -= Math.random() * data.data["value"]))
-    .catch((err) => console.log('err'))
-    .finally(() =>  console.log('ftest'));
+app.get("/parkmap", (req, res) => {
+  if (Date.now() - park_data.now > 300000) 
+    parkUpdate()
 
-  res.json({ value: Math.random() * randomArray[0] })
-});
-
-app.get("/parking-availability", async (req, res) => {
-  return res.json(lot_global);
+  res.json(park_data);  
 });
 
 app.post("/save", (req, res) => {
