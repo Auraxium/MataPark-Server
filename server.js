024b97be6445f08483aa23d3ef820f8bdf986197
SchoolSpice <91053214+SchoolSpice@users.noreply.github.com>
2023-05-11 13:40:00 -0700

    Update server.js

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ const lotStatusModel = mongoose.model("LotStatus", lotStatusSchema);
 
 const hostUrl = process.env.HURL || "http://localhost:8080";
 const URI =
-  "mongodb+srv://Lemond:z6WKxBTkHFuLUEKi@cluster0.cb5agdt.mongodb.net/?retryWrites=true&w=majority";
+  "mongodb+srv://Lemond:z6WKxBTkHFuLUEKi@cluster0.cb5agdt.mongodb.net/matapark?retryWrites=true&w=majority";
 console.error(new Date());
 
 app.use(
